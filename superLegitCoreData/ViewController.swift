928c8ca5b53e4b8e3a9202e5e1d7a02cfdd50ba4
Benjamin PRO <benjaminpoulsenpro@benjamins-macbook-air.local>
2019-01-14 08:06:23 -0700

    Initial Commit

---
+++ superLegitCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  superLegitCoreData
+//
+//  Created by Benjamin Poulsen PRO on 1/14/19.
+//  Copyright © 2019 Benjamin Poulsen PRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
